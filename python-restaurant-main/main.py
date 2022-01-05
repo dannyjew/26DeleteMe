a356cc51a034da69d114b9382f25eb7ff73e90a3
aelias-sparta <aelias@spartaglobal.com>
2022-01-05 16:40:12 +0000

    good, need fix 3 errors

--- python-restaurant-main/main.py
+++ python-restaurant-main/main.py
@@ -12,4 +12,29 @@ print(table01.get_total(0.15))
 print(table01.split_bill())
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
+
+# Run unit tests automatically
+#main(module='test_module', exit=False)
+
+# class Table:
+#
+#     def __init__(self, bill):
+#         self.bill = bill =[]
+#         self.mu = {"item": "", "price":0.0, "quantity":1}
+#
+#     def order(self, item:str, price:int, quantity:int=1 ):
+#         if self.mu["item"] == item and self.mu["price"] == price:
+#             self.mu["quantity"] = quantity
+#             self.bill.append(self.mu)
+#         else:
+#             self.mu["item"] = item
+#             self.mu["price"] = price
+#             self.mu["quantity"] = quantity
+#             self.bill.append(self.mu)
+#         #mu = {"item": item, "price": price, "quantity": quantity}
+#         return self.bill
+#
+# table01 = Table(5)
+# table01.order("Risotto", 12.50, 2)
+# print(table01.mu)
--- python-restaurant-main/restaurant.py
+++ python-restaurant-main/restaurant.py
@@ -1,2 +1,58 @@
 class Table:
-    pass
+
+    def __init__(self, people):
+        self.bill = []
+        self.people = people
+
+    def order(self, item:str, price:int, quantity:int=1 ):
+        menu = {"item": item, "price": price, "quantity": quantity}
+        if not self.bill:
+            self.bill.append(menu)
+        else:
+            for item in self.bill:
+                exist = False
+                # lets check if items already exisit
+                if menu["item"] == item['item'] and menu["price"] == item['price']:
+                    item['quantity'] += menu['quantity']
+                    exist = True
+                if exist:
+                    break
+                else:
+                    self.bill.append(menu)
+
+        return self.bill
+
+    def remove(self, item, price, quantity=1):
+        menu = {"item": item, "price": price, "quantity": quantity}
+        check = False
+        for item in self.bill:
+            if item["item"] == menu['item'] and item["price"] == menu['price']:
+                if ((item['quantity'] - menu["quantity"]) <= 0):
+                    del item
+                    check = False
+                    break # lets come out.
+                else:
+                    item['quantity'] -= menu["quantity"] # otherwise remove quantity
+                    check = True
+        return check
+
+    def get_subtotal(self):
+        sub_total = 0
+        for element in self.bill:
+            sub_total += element['price'] *element['quantity']
+        return sub_total
+
+    def get_total(self, s:float=0.10):
+        sub_total = self.get_subtotal()
+        service_charge = sub_total * s
+        total = sub_total + service_charge
+        return {"Sub Total":f"£{sub_total:02,.2f}", "Service Charge": f"£{service_charge:02,.2f}", "Total": f"£{total:02,.2f}"}
+
+    def split_bill(self):
+        sub_total = self.get_subtotal()
+        split = float(sub_total/self.people)
+
+        return split
+
+
+
