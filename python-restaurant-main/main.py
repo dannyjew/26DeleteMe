58f785448603d3de95a6d56dedc265cba4ad5909
JFereday <jacobfereday1234@gmail.com>
2022-01-05 14:32:28 +0000

    Completed Restaurant code

--- python-restaurant-main/main.py
+++ python-restaurant-main/main.py
@@ -11,5 +11,21 @@ print(table01.get_subtotal())
 print(table01.get_total(0.15))
 print(table01.split_bill())
 
+
+# table05 = Table(5)
+# table05.order('Food1', 10.00, 3)
+# table05.order('Food2', 20.00, 1)
+# table05.order('Food3', 0.60, 1)
+# actual = table05.get_total(0.15)
+
+
+# table06 = Table(6)
+# table06.order('Food1', 20.00, 3)
+# table06.order('Food2', 10.00, 1)
+# table06.order('Food3', 3.20, 1)
+# actual2 = table06.split_bill()
+
+
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- python-restaurant-main/restaurant.py
+++ python-restaurant-main/restaurant.py
@@ -1,2 +1,71 @@
+import math
+
 class Table:
-    pass
+    def __init__(self,diners):
+        self.diners = diners
+        self.bill = []
+        
+        
+    def order(self,name,price,quantity=1):
+        if self.bill == []:
+            self.bill.append({"item":name,"price":price,"quantity":quantity})
+            return
+
+
+        for i in range(0,len(self.bill)):
+            if self.bill[i]["item"] == str(name):
+                self.bill[i]['quantity'] += quantity
+                return
+                    
+        self.bill.append({"item":name,"price":price,"quantity":quantity})
+        
+
+
+
+        
+    def remove(self,name,price,quantity):
+        try:
+            self.bill.remove({"item":name,"price":price,"quantity":quantity})
+        except:
+            for i in range(0,len(self.bill)):
+                if self.bill[i]['item'] == name:
+                    self.bill[i]['quantity'] -= quantity
+                    pass
+
+    
+
+    
+  
+    
+    def bill(self):
+        print(self.bill)
+
+
+
+    
+    def get_subtotal(self):
+        
+        sub_total = 0
+        for i in range(0,len(self.bill)):
+            sub_total += self.bill[i]['price'] * self.bill[i]['quantity']
+        
+        return sub_total
+
+
+        
+    def get_total(self,service_charge_percentage=0.1):
+        
+        sub_total = self.get_subtotal()
+        service_charge = round(sub_total * float(service_charge_percentage),2)
+        
+        return {"Sub Total": "£"+('%.2f' %sub_total), "Service Charge": "£"+str(service_charge), "Total": "£"+str(sub_total+service_charge)}
+        
+
+
+        
+    def split_bill(self):
+        
+        sub_total = self.get_subtotal()
+        split = math.ceil((sub_total / self.diners)*100)/100
+        
+        return split
